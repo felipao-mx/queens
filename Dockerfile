5355acbcbc33b6123c0f554e71f82ae4528e5e53
Felipe López <flh.1989@gmail.com>
2018-10-05 08:27:33 -0500

    -configuracion docker compose -datos de conexion parametrizados -fix dockerfile: dependencias requeridas para psycopg2

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,12 @@ COPY . /app
 
 WORKDIR /app
 
+RUN apk update && \
+ apk add python3 postgresql-libs && \
+ apk add --virtual .build-deps gcc python3-dev musl-dev postgresql-dev && \
+ python3 -m pip install -r requirements.txt --no-cache-dir && \
+ apk --purge del .build-deps
+ 
 RUN pip install -r requirements.txt
 
 CMD ["python", "./queens/app.py"]
--- queens/repositories/benchmarkrepository.py
+++ queens/repositories/benchmarkrepository.py
@@ -2,9 +2,18 @@ from sqlalchemy import create_engine
 from sqlalchemy import Column, String, Integer, DateTime, Interval, func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-dbString = "postgresql+psycopg2://postgres:admin@192.168.99.100:5432/queensBenchmark"
 
+user = os.environ['POSTGRES_USER']
+pwd = os.environ['POSTGRES_PASSWORD']
+db = os.environ['POSTGRES_DB']
+host = 'postgresdb'
+port = '5432'
+
+# dbString = "postgresql+psycopg2://queens:queens@192.168.99.100:5432/queensdb"
+dbString = 'postgresql+psycopg2://%s:%s@%s:%s/%s' % (user, pwd, host, port, db)
+print("connection string: " + dbString)
 db = create_engine(dbString)
 
 base = declarative_base()
