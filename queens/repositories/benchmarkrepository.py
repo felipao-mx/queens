ac9e2e4241561db974f7779709875bd42b2314b8
Felipe López <flh.1989@gmail.com>
2018-10-06 13:41:43 -0500

    actualizar valor de id

--- queens/repositories/benchmarkrepository.py
+++ queens/repositories/benchmarkrepository.py
@@ -10,3 +10,4 @@ class BenchmarkRepository:
             name=benchmark.name, start=benchmark.startDate, end=benchmark.endDate)
         self.session.add(register)
         self.session.commit()
+        benchmark.id = register.benchmarkId
